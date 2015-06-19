28c0e3c7b189ddc767f8cf1b07a73044ae71c511
Dan Burnett <dburnett@voxeo.com>
2015-06-18 21:41:10 -0400

    updated status and publisher for ES6 reference

--- getusermedia.js
+++ getusermedia.js
@@ -93,8 +93,8 @@ var respecConfig = {
        title:  "ECMAScript 2015",
        href: "http://wiki.ecmascript.org/lib/exe/fetch.php?id=harmony%3Aspecification_drafts&cache=cache&media=harmony:ecma-262_6th_edition_final_draft_-04-14-15.pdf",
        authors: ["Allen Wirfs-Brock"],
-       status: "Draft Standard",
-       publisher: "ECMA"
+       status: "Standard",
+       publisher: "Ecma International"
      }
    }
 
